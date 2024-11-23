bb235368e6f5200af9510f5ac842b4d63bed9617
RannD101 <randibisnis2@gmail.com>
2024-11-23 22:46:52 +0700

    Add files via upload

--- server/app.js
+++ server/app.js
@@ -1,80 +1,79 @@
-/*
-   * by balzz
-   * dont delete my wm
-   * follow more instagram: @iqstore78
-*/
+const express = require("express");
+const session = require("express-session");
+const path = require("path");
+const bodyParser = require("body-parser");
+const { limit, checkBanned } = require("../declaration/rateLimit.jsx");
+const isAuthenticated = require("../declaration/autentikasi.jsx");
 
-const express = require("express")
-const axios = require("axios")
-const session = require("express-session")
-const path = require("path")
-const bodyParser = require('body-parser')
-const { limit, checkBanned } = require("../declaration/rateLimit.jsx")
-const isAuthenticated = require("../declaration/autentikasi.jsx")
+// Inisialisasi aplikasi Express
+const app = express();
 
-const app = express()
-app.use(checkBanned)
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(session({
-    secret: 'komtolllll',
+// Middleware global
+app.use(checkBanned);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "komtolllll", // Gunakan secret yang lebih aman untuk produksi
     resave: false,
     saveUninitialized: true,
-    cookie: { maxAge: 3600000 } 
-}))
+    cookie: { maxAge: 3600000 }, // 1 jam
+  })
+);
 
-// function isAuthenticated(req, res, next) {
-//     if (req.session && req.session.email) {
-//         next()
-//     } else {
-//         res.redirect("/login")
-//     }
-// }
-
-/* !=== PAGE ===! */
+/* === Rute Halaman === */
 app.get("/", limit, (req, res) => {
-    res.sendFile(path.join(__dirname, "../pages/404.html"))
-})
+  res.sendFile(path.join(__dirname, "../pages/404.html"));
+});
 
 app.get("/login", limit, (req, res) => {
-    res.sendFile(path.join(__dirname, "../pages/login.html"))
-})
+  res.sendFile(path.join(__dirname, "../pages/login.html"));
+});
 
 app.get("/profile", limit, isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, "../pages/profile.html"))
-})
+  res.sendFile(path.join(__dirname, "../pages/profile.html"));
+});
 
-/* = ENDPOINT FITURE = */
-app.post('/register', (req, res) => {
-    require("../declaration/register.jsx")(req, res)
-})
+/* === Rute Endpoint Fitur === */
+app.post("/register", (req, res) => {
+  const registerHandler = require("../declaration/register.jsx");
+  registerHandler(req, res);
+});
 
-app.post('/login', (req, res) => {
-    require("../declaration/login.jsx")(req, res)
-})
+app.post("/login", (req, res) => {
+  const loginHandler = require("../declaration/login.jsx");
+  loginHandler(req, res);
+});
 
 app.get("/logout", (req, res) => {
-    require("../declaration/logout.jsx")(req, res)
-})
+  const logoutHandler = require("../declaration/logout.jsx");
+  logoutHandler(req, res);
+});
 
 app.get("/prof", isAuthenticated, (req, res) => {
-    require("../declaration/profile.jsx")(req, res)
-})
+  const profileHandler = require("../declaration/profile.jsx");
+  profileHandler(req, res);
+});
 
+/* === Rute Fitur Unduhan === */
 app.get("/blekbok", limit, async (req, res) => {
-    require("../pages/fitures/blackbox.js")(req, res)
-})
+  const blackboxHandler = require("../pages/fitures/blackbox.js");
+  await blackboxHandler(req, res);
+});
 
 app.get("/tiktokdl", limit, async (req, res) => {
-    require("../pages/fitures/tiktok.js")(req, res)
-})
+  const tiktokHandler = require("../pages/fitures/tiktok.js");
+  await tiktokHandler(req, res);
+});
 
 app.get("/instagramdl", limit, async (req, res) => {
-    require("../pages/fitures/instagram.js")(req, res)
-})
+  const instagramHandler = require("../pages/fitures/instagram.js");
+  await instagramHandler(req, res);
+});
 
-app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "../pages/404.html"))
-})
+/* === Penanganan 404 === */
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "../pages/404.html"));
+});
 
-module.exports = app
+module.exports = app;
