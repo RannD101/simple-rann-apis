88d094462a513edbde0d39d9ff682c613aeadcb9
RannD101 <randibisnis2@gmail.com>
2024-11-24 14:30:09 +0700

    Update instagram.js

--- pages/fitures/instagram.js
+++ pages/fitures/instagram.js
@@ -1,97 +1,31 @@
-const cheerio = require("cheerio");
+const axios = require("axios")
+const allowedApiKeys = require("../../declaration/arrayKey.jsx")
 
-const igdl = async (url) => {
-  try {
-    const got = (await import("got")).default;
-
-    // Fungsi decoding dan scraping tetap sama
-    function decodeSnapApp(args) {
-      let [h, u, n, t, e, r] = args;
-      function decode(d, e, f) {
-        const g = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+/".split("");
-        let h = g.slice(0, e);
-        let i = g.slice(0, f);
-        let j = d.split("").reverse().reduce(function (a, b, c) {
-          if (h.indexOf(b) !== -1)
-            return (a += h.indexOf(b) * Math.pow(e, c));
-        }, 0);
-        let k = "";
-        while (j > 0) {
-          k = i[j % f] + k;
-          j = (j - (j % f)) / f;
-        }
-        return k || "0";
-      }
-      let decodedString = "";
-      for (let i = 0, len = h.length; i < len; i++) {
-        let s = "";
-        while (h[i] !== n[e]) {
-          s += h[i];
-          i++;
-        }
-        for (let j = 0; j < n.length; j++) s = s.replace(new RegExp(n[j], "g"), j.toString());
-        decodedString += String.fromCharCode(decode(s, e, 10) - t);
-      }
-      return decodeURIComponent(encodeURIComponent(decodedString));
-    }
-
-    function getEncodedSnapApp(data) {
-      return data.split("decodeURIComponent(escape(r))}(")[1].split("))")[0].split(",").map((v) => v.replace(/"/g, "").trim());
-    }
-
-    function getDecodedSnapSave(data) {
-      return data.split('getElementById("download-section").innerHTML = "')[1].split('"; document.getElementById("inputData").remove(); ')[0].replace(/\\?/g, "");
-    }
-
-    function decryptSnapSave(data) {
-      return getDecodedSnapSave(decodeSnapApp(getEncodedSnapApp(data)));
-    }
-
-    const html = await got
-      .post("https://snapsave.app/action.php?lang=id", {
-        headers: {
-          accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-          "content-type": "application/x-www-form-urlencoded",
-          origin: "https://snapsave.app",
-          referer: "https://snapsave.app/id",
-          "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
-        },
-        form: { url },
-      })
-      .text();
-
-    const decode = decryptSnapSave(html);
-    const $ = cheerio.load(decode);
-    const results = [];
-
-    if ($("table.table").length || $("article.media > figure").length) {
-      const thumbnail = $("article.media > figure").find("img").attr("src");
-      $("tbody > tr").each((_, el) => {
-        const $el = $(el);
-        const $td = $el.find("td");
-        const resolution = $td.eq(0).text();
-        let _url = $td.eq(2).find("a").attr("href") || $td.eq(2).find("button").attr("onclick");
-        if (/get_progressApi/ig.test(_url || "")) {
-          _url = /get_progressApi'(.*?)'/.exec(_url || "")?.[1] || _url;
-        }
-        results.push({ resolution, thumbnail, url: _url });
-      });
-    } else {
-      $("div.download-items__thumb").each((_, tod) => {
-        const thumbnail = $(tod).find("img").attr("src");
-        $("div.download-items__btn").each((_, ol) => {
-          let _url = $(ol).find("a").attr("href");
-          if (!/https?:\/\//.test(_url || "")) _url = `https://snapsave.app${_url}`;
-          results.push({ thumbnail, url: _url });
-        });
-      });
-    }
-
-    if (!results.length) return { msg: "Blank data" };
-    return { data: results };
-  } catch (e) {
-    return { msg: e.message };
+module.exports = async (req, res) => {
+  let urls = req.query.urls
+  if (!urls) {
+    return res.status(400).json({
+      error: "Url Ig Nya Mana?"
+    })
   }
-};
 
-module.exports = igdl;
+  if (!apiKey || !allowedApiKeys.includes(apiKey)) {
+    return res.status(403).json({
+      error: "Input Parameter Apikey !"
+    })
+  }
+
+  let url = `https://api.agatz.xyz/api/instagram?url=${urls}`
+
+  try {
+    const response = await axios.get(url)
+    const videoUrl = response.data.data.url_list[0]
+    res.status(200).json({
+      videoUrl
+    })
+  } catch (e) {
+    res.status(500).json({
+      error: "Ada masalah, coba lagi nanti"
+    })
+  }
+}
