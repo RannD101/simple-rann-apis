d96c88a408618c6b2874f41e25162d708632faef
RannD101 <randibisnis2@gmail.com>
2024-11-24 15:52:51 +0700

    Update app.js

--- server/app.js
+++ server/app.js
@@ -6,8 +6,6 @@ const bodyParser = require('body-parser');
 const { limit, checkBanned } = require("../declaration/rateLimit.jsx");
 const isAuthenticated = require("../declaration/autentikasi.jsx");
 const TikWM = require("../pages/fitures/tiktok.js"); // Pastikan path ke TikWM.js sesuai
-const cors = require("cors");
-app.use(cors());
 
 const app = express();
 
