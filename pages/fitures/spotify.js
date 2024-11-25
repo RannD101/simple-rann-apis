db8a582d65e1c033ab15166ad6e473e7f52aa394
RannD101 <randibisnis2@gmail.com>
2024-11-25 11:43:55 +0700

    Create spotify.js

---
+++ pages/fitures/spotify.js
@@ -0,0 +1,43 @@
+const axios = require("axios");
+
+module.exports = async (req, res) => {
+    const { judul, apiKey } = req.query;
+
+    if (!judul) {
+        return res.status(400).json({ error: "Masukan Judul Lagu" });
+    }
+    if (!apiKey) {
+        return res.status(403).json({ error: "API Key dibutuhkan" });
+    }
+
+    try {
+        const response = await axios.get(`https://api.agatz.xyz/api/spotify`, {
+            params: {
+                message,
+            },
+        });
+
+        if (!response.data || response.data.error) {
+            return res.status(500).json({
+                error: "Gagal mengambil data dari API eksternal",
+                details: response.data.error || "Tidak ada data yang diterima",
+            });
+        }
+
+        res.status(200).json({
+            message: "Data berhasil diambil",
+            data: {
+                status: 200,
+                creator: "Rann",
+                data: response.data.data,
+            },
+        });
+    } catch (error) {
+        console.error("Error Instagram API:", error.message || error);
+
+        res.status(500).json({
+            error: "Terjadi kesalahan internal",
+            details: error.message || "Kesalahan tidak diketahui",
+        });
+    }
+};
